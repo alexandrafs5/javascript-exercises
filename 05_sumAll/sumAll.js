4a824a3da618a4ca6d813693d5a4e6d902cd9641
alexandrafs5 <alexandra051009@gmail.com>
2024-05-27 12:07:16 -0700

    Finish the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    let result = 0;
+    
+    if (numberOne < numberTwo) {
+        for (let i = numberOne; i <= numberTwo; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = numberTwo; i <= numberOne; i++) {
+            result += i;
+        }
+    }
 
+    if (numberOne > 0 && numberTwo > 0 && Number.isInteger(numberOne) && Number.isInteger(numberTwo)) {
+        return result;
+    }
+    return "ERROR";
 };
+
+sumAll(1, 5);
 
 // Do not edit below this line
 module.exports = sumAll;
