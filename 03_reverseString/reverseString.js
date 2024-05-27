b65c5eb04937b2daaa2cbbe455227208fc1d4132
alexandrafs5 <alexandra051009@gmail.com>
2024-05-27 00:58:00 -0700

    Finish the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string = string.split("");
+    string = string.reverse();
+    string = string.join("");
+    return string;
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
