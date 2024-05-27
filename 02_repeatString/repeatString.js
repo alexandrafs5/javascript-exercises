26c6906854f0835211e66023ac07a8ed8dd6d51a
alexandrafs5 <alexandra051009@gmail.com>
2024-05-27 00:49:27 -0700

    Finish the last part

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,9 +3,15 @@ const repeatString = function(string, number) {
     for (let i = 0; i < number; i++) {
         result += string;
     }
+
+    if (number < 0) {
+        return "ERROR";
+    }
+    
     return result;
 };
 
 repeatString("hey", 3);
+
 // Do not edit below this line
 module.exports = repeatString;
