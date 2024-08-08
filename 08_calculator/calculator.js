1fff09f3f70f2fc4fad5d1a20be3064c9f3a2b68
alexandrafs5 <alexandra051009@gmail.com>
2024-08-07 18:51:53 -0700

    finish the factorial section of the exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,13 @@ const power = function(a, b) {
 	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0 || a === 1)
+    return 1;
+  for (let i = a - 1; i >= 1; i--) {
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
