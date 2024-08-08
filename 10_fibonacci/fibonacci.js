2eb34e7dbf4f0c6b719a3661f4a31e864ba83c99
alexandrafs5 <alexandra051009@gmail.com>
2024-08-08 00:48:23 -0700

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [0, 1];
+    for (let i = 2; i < n + 1; i++){
+        arr.push(arr[i - 2] + arr[i -1]);
+    }
+    if (n < 0) {
+        return "OOPS";
+    } else {
+        return arr[n];
+    }
 };
 
 // Do not edit below this line
